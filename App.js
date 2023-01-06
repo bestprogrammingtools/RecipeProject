30d463669f8d870999cc23103f0ffd4944e3d514
bestprogrammingtools <120991824+bestprogrammingtools@users.noreply.github.com>
2023-01-06 18:56:38 -0500

    gakvetili3

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import styles from './styles';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <Text>Chveni Pirveli Tekstis Komponenti</Text>
+      <Text style={[styles.textStyle, styles.textStyleBackground]}>
+        Chveni Pirveli Tekstis Komponenti
+      </Text>
     </SafeAreaView>
   );
 };
---
+++ styles.js
@@ -0,0 +1,6 @@
+import {StyleSheet} from 'react-native';
+const styles = StyleSheet.create({
+  textStyle: {color: 'red'},
+  textStyleBackground: {backgroundColor: 'black'},
+});
+export default styles;
