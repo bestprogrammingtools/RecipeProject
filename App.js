90f87ba3230efe6abc5d6efd7f790b3cbb3329e1
bestprogrammingtools <120991824+bestprogrammingtools@users.noreply.github.com>
2023-01-06 18:56:01 -0500

    gakvetili2

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
 
 const App = () => {
-  return null;
+  return (
+    <SafeAreaView>
+      <Text>Chveni Pirveli Tekstis Komponenti</Text>
+    </SafeAreaView>
+  );
 };
 
 export default App;
